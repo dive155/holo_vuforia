a050090fe627cd28bc7a3cdc5a9f80cfd03c689e
PETERSBURG\Dmitrii_Valiaev1 <Dmitrii_Valiaev1@epam.com>
2018-01-09 19:39:15 +0300

    Improved position/rotation interpolation

---
+++ Assets/Scripts/Placing/MachinePlacer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachinePlacer : MonoBehaviour {
+
+    [SerializeField] private Transform pivot1;
+    [SerializeField] private Transform pivot2;
+
+    void Update()
+    {
+        this.transform.position = Vector3.Lerp(pivot1.position, pivot2.position, 0.5f);
+        this.transform.rotation = Quaternion.Lerp(pivot1.rotation, pivot2.rotation, 0.5f);
+    }
+
+
+}
