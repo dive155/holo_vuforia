3eececf5f60cb4f8dfda445d53e7e9a4ade14ee5
dive155 <divedimon@mail.ru>
2018-02-11 00:46:49 +0300

    Added rig prefab made on 5.5.0

---
+++ MDLs/Exported/Scripts/RigController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigController : MonoBehaviour {
+
+
+    [SerializeField] private ArmAnimator armAnimator;
+    [SerializeField] private BeltSpinner beltSpinner;
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            armAnimator.MoveUp();
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            armAnimator.MoveDown();
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            beltSpinner.SwitchBeltMoving();
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            armAnimator.MoveOut();
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            armAnimator.MoveIn();
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            armAnimator.StopMoving();
+        }
+	}
+}
