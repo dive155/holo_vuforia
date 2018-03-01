9e03bbfc71218ed2850305173a42055482d0732c
Dive <divedimon@mail.ru>
2018-03-01 14:05:28 +0300

    Started importing the rig

---
+++ Assets/Data/Exported/Scripts/ArmAnimator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmAnimator : MonoBehaviour {
+
+    [SerializeField] private Animator armAnimator;
+    [SerializeField] private Animator boxAnimator;
+
+    public void MoveUp()
+    {
+        armAnimator.SetTrigger("MoveUp");
+    }
+
+    public void MoveDown()
+    {
+        armAnimator.SetTrigger("MoveDown");
+    }
+
+    public void MoveOut()
+    {
+        boxAnimator.SetTrigger("MoveOut");
+    }
+
+    public void MoveIn()
+    {
+        boxAnimator.SetTrigger("MoveIn");
+    }
+
+    public void StopMoving()
+    {
+        boxAnimator.SetTrigger("StopMoving");
+    }
+
+
+
+}
---
+++ Assets/Data/Exported/Scripts/BeltSpinner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltSpinner : MonoBehaviour {
+
+    [SerializeField] private Transform upperWheel;
+    [SerializeField] private Transform lowerWheel;
+
+    [SerializeField] private float upSpeed = 1;
+    [SerializeField] private float downSpeed = 1.5f;
+
+    [SerializeField] private Renderer beltRend;
+    [SerializeField] private float beltSpeed = 1;
+
+    [SerializeField] private bool active;
+
+    public void SetBeltMoving(bool val)
+    {
+        active = val;
+    }
+
+    public void SwitchBeltMoving()
+    {
+        active = !active;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        if (active)
+        {
+            upperWheel.Rotate(0, 0, upSpeed);
+            lowerWheel.Rotate(0, 0, downSpeed);
+            beltRend.material.mainTextureOffset = new Vector2((beltRend.material.mainTextureOffset.x + beltSpeed)%1, beltRend.material.mainTextureOffset.y);
+        }
+	}
+}
